de7a231d593024c07d297ff3af40844463a81bd0
Jiradet Ounjai <jiradet.jd@gmail.com>
2022-09-16 22:02:23 +0700

    move pages to root

---
+++ _app/immutable/chunks/0-605365ae.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/layout.svelte-d13d80e7.js";export{t as component};
---
+++ _app/immutable/modules/pages/_page.ts-6703f682.js
@@ -0,0 +1,1 @@
+import{F as o,l as a,p as s}from"../../chunks/_page-d677ed0e.js";export{o as FetchFromJson,a as load,s as prerender};
